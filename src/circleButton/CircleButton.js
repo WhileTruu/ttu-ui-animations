2581c7fa62d4141c2983339210a5176b48b288ab
taaniel <taaniel.tyyr@codelab.ee>
2017-03-22 19:18:46 +0200

    putton circle spin

--- src/circleButton/CircleButton.js
+++ src/circleButton/CircleButton.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './circleButton.css'
 import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
-const CircleButton = () => (
-  <div className="btn-circle-container col-md-2">
-    <button className="btn btn-circle btn-primary">
-      <i className="fa fa-comments spin" aria-hidden="true" />
-    </button>
-  </div>
-)
+class CircleButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.state.spin = false
+  }
+  spinDone() {
+    this.setState({ spin: !this.state.spin })
+  }
+  render() {
+    return (
+      <div className="btn-circle-container col-md-2">
+        <button
+          onClick={() => this.setState({ spin: true })}
+          className="btn btn-circle btn-primary"
+        >
+          <i
+            aria-hidden="true"
+            className={this.state.spin ? 'fa fa-comments spin' : 'fa fa-comments'}
+          />
+        </button>
+      </div>
+    )
+  }
+}
 
 export default CircleButton
