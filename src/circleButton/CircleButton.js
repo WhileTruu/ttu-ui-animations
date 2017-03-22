37be2894b4a4ab79da402c6702bbefe1d90c70b3
taaniel <taaniel.tyyr@codelab.ee>
2017-03-22 18:07:32 +0200

    button icon spin

--- src/circleButton/CircleButton.js
+++ src/circleButton/CircleButton.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './circleButton.css'
 import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
-const CircleButton = () => (
-  <div className="btn-circle-container col-md-2">
-    <button className="btn btn-circle btn-primary">
-      <i className="fa fa-comments" aria-hidden="true" />
-    </button>
-  </div>
-)
+class CircleButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.state.spin = false
+    this.spinDone = this.spinDone.bind(this)
+  }
+  componentDidMount() {
+    const elm = this.button
+    if (elm) {
+      elm.addEventListener('animationend', this.spinDone)
+    }
+  }
+  componentWillUnmount() {
+    const elm = this.button
+    elm.removeEventListener('animationend', this.spinDone)
+  }
+  spinDone() {
+    this.setState({ spin: false })
+  }
+  render() {
+    const spin = this.state.spin
+    return (
+      <div className="btn-circle-container col-md-2">
+        <button
+          onClick={() => this.setState({ spin: true })}
+          className="btn btn-circle btn-primary"
+        >
+          <i
+            aria-hidden="true"
+            className={spin ? 'fa fa-comments spin' : 'fa fa-comments'}
+          />
+        </button>
+      </div>
+    )
+  }
+}
 
 export default CircleButton
