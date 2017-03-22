68812e57e63d131775969b705fdc8d03b2ef8344
Marten Truu <marten.truu@gmail.com>
2017-03-22 14:47:16 +0200

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App'
+import './index.css'
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
