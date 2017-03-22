7a1572b84a234172b3d7f37634054a9c1fd5c27b
taaniel <taaniel.tyyr@codelab.ee>
2017-03-22 15:19:08 +0200

    button position

--- src/circleButton/CircleButton.js
+++ src/circleButton/CircleButton.js
@@ -3,7 +3,7 @@ import './circleButton.css'
 import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 const CircleButton = () => (
-  <div className="circleButton">
+  <div className="btn-circle-container col-md-2">
     <button className="btn btn-circle btn-primary">
       <i className="fa fa-comments" aria-hidden="true" />
     </button>
