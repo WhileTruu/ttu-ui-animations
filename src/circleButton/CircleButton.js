3759fc2b3197b5d7ff057293d32851e13a7dc9f0
taaniel <taaniel.tyyr@codelab.ee>
2017-03-22 15:05:32 +0200

    button icon

--- src/circleButton/CircleButton.js
+++ src/circleButton/CircleButton.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './circleButton.css'
-
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 const CircleButton = () => (
   <div className="circleButton">
-    <button className="btn btn-circle btn-primary">Yolo</button>
+    <button className="btn btn-circle btn-primary">
+      <i className="fa fa-comments" aria-hidden="true" />
+    </button>
   </div>
 )
 
